23ead409fdc79349fe1226f7c3301977a2ce9b71
Junseo Bae <ferrater1013@gmail.com>
2025-10-25 23:46:36 +0900

    feat(CarName): 자동차 이름 검증 메서드 추가
    - 최대 길이 검증 메서드 추가
    - blank 검증 메서드 추가

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,7 +1,35 @@
 package racingcar.domain;
 
+import racingcar.error.ErrorMessage;
+
 public class CarName {
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+
+    private final String name;
+
     public CarName(final String name) {
-        
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(final String name) {
+        validateNotBlank(name);
+        validateLength(name);
+    }
+
+    private void validateNotBlank(final String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.EMPTY_CAR_NAME.message());
+        }
+    }
+
+    private void validateLength(final String name) {
+        if (name.length() > MAX_CAR_NAME_LENGTH) {
+            throw new IllegalArgumentException(ErrorMessage.EXCEED_CAR_NAME_MAX_LENGTH.message());
+        }
+    }
+
+    public String value() {
+        return name;
     }
 }
