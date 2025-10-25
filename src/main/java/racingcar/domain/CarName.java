248b347a7bc20f36433e90c53c5492418d2779f5
Junseo Bae <ferrater1013@gmail.com>
2025-10-25 23:37:56 +0900

    test(CarName): 자동차 이름 검증 테스트 추가
    - 자동차 이름의 최대 길이 검증을 위한 테스트 추가
    - 비어있는 자동차 이름 검증을 위한 테스트 추가

---
+++ src/main/java/racingcar/domain/CarName.java
@@ -0,0 +1,7 @@
+package racingcar.domain;
+
+public class CarName {
+    public CarName(final String name) {
+        
+    }
+}
---
+++ src/test/java/racingcar/domain/CarNameTest.java
@@ -0,0 +1,26 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.error.ErrorMessage;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CarNameTest {
+
+    @DisplayName("자동차 이름 길이가 최대 길이를 초과할 수 없다.")
+    @Test
+    void shouldThrowsException_whenLengthOverFive() {
+        assertThatThrownBy(() -> new CarName("가나다라마바사"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.EXCEED_CAR_NAME_MAX_LENGTH.message());
+    }
+
+    @DisplayName("자동차 이름이 비어있을 수 없다.")
+    @Test
+    void shouldThrowsException_whenNameIsEmpty() {
+        assertThatThrownBy(() -> new CarName(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.EMPTY_CAR_NAME.message());
+    }
+}
