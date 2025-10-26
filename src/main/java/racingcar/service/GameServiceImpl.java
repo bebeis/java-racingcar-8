9e1ba96dce7443a75d1fd77abd08b0e91092d651
Junseo Bae <ferrater1013@gmail.com>
2025-10-27 05:43:05 +0900

    feat: GameServiceImpl 클래스 추가
    - 자동차 이름 설정, 라운드 진행, 우승자 결정 기능 구현
    - CarsRepository를 통해 자동차 목록 관리 및 스냅샷 생성 반환

---
+++ src/main/java/racingcar/service/GameServiceImpl.java
@@ -0,0 +1,57 @@
+package racingcar.service;
+
+import racingcar.domain.car.Car;
+import racingcar.domain.car.CarName;
+import racingcar.domain.car.Cars;
+import racingcar.domain.round.Round;
+import racingcar.domain.strategy.MoveStrategy;
+import racingcar.repository.CarsRepository;
+import racingcar.service.dto.CarStatus;
+import racingcar.service.dto.RoundSnapShot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameServiceImpl implements GameService {
+    private MoveStrategy strategy;
+    private CarsRepository repository;
+
+    public GameServiceImpl(final MoveStrategy strategy, final CarsRepository repository) {
+        this.strategy = strategy;
+        this.repository = repository;
+    }
+
+    @Override
+    public void setUpCarNames(final List<String> carNames) {
+        List<Car> carList = carNames.stream()
+                .map(CarName::new)
+                .map(Car::new)
+                .toList();
+
+        Cars cars = new Cars(carList);
+        repository.save(cars);
+    }
+
+    @Override
+    public List<RoundSnapShot> playRounds(final int tryCount) {
+        Cars cars = repository.findCars();
+
+        List<RoundSnapShot> snapShots = new ArrayList<>();
+        Round currentRound = Round.from(tryCount);
+        while (!currentRound.isFinished()) {
+            cars.moveAll(strategy);
+            snapShots.add(RoundSnapShot.from(cars));
+            currentRound = currentRound.next();
+        }
+
+        return snapShots;
+    }
+
+    @Override
+    public List<CarStatus> getWinners() {
+        List<Car> cars = repository.findCars().determineWinners();
+        return cars.stream()
+                .map(CarStatus::from)
+                .toList();
+    }
+}
