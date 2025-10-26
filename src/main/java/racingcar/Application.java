ce2939c8723783c530e28198c3e9c217e3cd519e
Junseo Bae <ferrater1013@gmail.com>
2025-10-27 06:50:30 +0900

    feat: Application bootstrap 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.domain.strategy.RandomMoveStrategy;
+import racingcar.repository.CarsMemoryRepository;
+import racingcar.service.GameService;
+import racingcar.service.GameServiceImpl;
+import racingcar.view.ConsoleOutputView;
+import racingcar.view.ConsoleUserInterface;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new ConsoleUserInterface();
+        OutputView outputView = new ConsoleOutputView();
+        GameService gameService = new GameServiceImpl(new RandomMoveStrategy(), new CarsMemoryRepository());
+        GameController gameController = new GameController(inputView, outputView, gameService);
+        gameController.run();
     }
 }
