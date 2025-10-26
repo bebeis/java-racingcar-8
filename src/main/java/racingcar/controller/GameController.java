2478e02aa98de10505ed1c92a1ef48d580e77e56
Junseo Bae <ferrater1013@gmail.com>
2025-10-27 04:37:16 +0900

    feat: 게임 전체 흐름을 제어하는 GameController 구현
    - 자동차 이름과 시도 횟수를 입력
    - 라운드를 진행 및 스냅샷 생성
    - 라운드 결과와 우승자 출력

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,34 @@
+package racingcar.controller;
+
+import racingcar.service.GameService;
+import racingcar.service.dto.RoundSnapShot;
+import racingcar.service.dto.WinnerResponse;
+import racingcar.util.CommaSeparator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class GameController {
+    private InputView inputView;
+    private OutputView outputView;
+    private GameService gameService;
+
+    public GameController(final InputView inputView, final OutputView outputView, final GameService gameService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.gameService = gameService;
+    }
+
+    public void run() {
+        List<String> carNames = CommaSeparator.split(inputView.readCarNames());
+        gameService.setUpCarNames(carNames);
+
+        int tryCount = inputView.readTryCount();
+        List<RoundSnapShot> roundSnapShots = gameService.playRounds(tryCount);
+        outputView.showRoundSnapShots(roundSnapShots);
+
+        List<WinnerResponse> winners = gameService.getWinners();
+        outputView.showWinners(winners);
+    }
+}
