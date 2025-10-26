482dc51996a3ef43db292e857ec2138c70930468
Junseo Bae <ferrater1013@gmail.com>
2025-10-26 22:09:36 +0900

    feat(Round): Round 도메인 규칙 구현 메서드 추가
    - 다음 라운드로 진행
    - 게임 종료 여부 판정
    - 초기 시도 횟수 검증

---
+++ src/main/java/racingcar/domain/Round.java
@@ -0,0 +1,45 @@
+package racingcar.domain;
+
+import static racingcar.error.ErrorMessage.CANNOT_PROCEED_WHEN_FINISHED;
+import static racingcar.error.ErrorMessage.NON_POSITIVE_TRY_COUNT;
+
+public class Round {
+    private static final int MINIMUM_TRY_COUNT = 1;
+    private static final int MINIMUM_REMAINING_COUNT = 0;
+
+    private final int remainingCount;
+
+    private Round(final int remainingCount) {
+        validateRemainingCountRange(remainingCount);
+        this.remainingCount = remainingCount;
+    }
+
+    private void validateRemainingCountRange(final int remainingCount) {
+        if (remainingCount < MINIMUM_REMAINING_COUNT) {
+            throw new IllegalStateException(CANNOT_PROCEED_WHEN_FINISHED.message());
+        }
+    }
+
+    public static Round from(final int tryCount) {
+        validateTryCountRange(tryCount);
+        return new Round(tryCount);
+    }
+
+    private static void validateTryCountRange(final int tryCount) {
+        if (tryCount < MINIMUM_TRY_COUNT) {
+            throw new IllegalArgumentException(NON_POSITIVE_TRY_COUNT.message());
+        }
+    }
+
+    public Round next() {
+        return new Round(remainingCount - 1);
+    }
+
+    public int getValue() {
+        return remainingCount;
+    }
+
+    public boolean isFinished() {
+        return remainingCount == MINIMUM_REMAINING_COUNT;
+    }
+}
--- src/main/java/racingcar/error/ErrorMessage.java
+++ src/main/java/racingcar/error/ErrorMessage.java
@@ -6,7 +6,9 @@ public enum ErrorMessage {
     NEGATIVE_MOVE_STEP("음의 방향으로 이동할 수 없습니다."),
     EXCEED_CAR_NAME_MAX_LENGTH("자동차 이름의 최대 길이를 초과하였습니다."),
     EMPTY_CAR_NAME("자동차 이름은 비어있을 수 없습니다."),
-    EMPTY_CAR_LIST("자동차 목록은 비어있을 수 없습니다.");
+    EMPTY_CAR_LIST("자동차 목록은 비어있을 수 없습니다."),
+    NON_POSITIVE_TRY_COUNT("시도 횟수는 0 이하일 수 없습니다."),
+    CANNOT_PROCEED_WHEN_FINISHED("종료된 이후에 라운드를 진행할 수 없습니다");
 
     private final String message;
 
