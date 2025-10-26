3e5cbd5a7d1d8e5fbc438e71ccd2bb984c2e1c06
Junseo Bae <ferrater1013@gmail.com>
2025-10-27 05:49:08 +0900

    feat(CarsRepository): 저장 및 조회 기능 구현

---
+++ src/main/java/racingcar/repository/CarsMemoryRepository.java
@@ -0,0 +1,18 @@
+package racingcar.repository;
+
+import racingcar.domain.car.Cars;
+
+public class CarsMemoryRepository implements CarsRepository {
+    
+    private Cars cars;
+
+    @Override
+    public void save(final Cars cars) {
+        this.cars = cars;
+    }
+
+    @Override
+    public Cars findCars() {
+        return cars;
+    }
+}
